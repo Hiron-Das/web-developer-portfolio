6c2902cc3af38864d3fb5bcfe5abdf7fbae2fb5d
Hiron Das <hiron9340@gmail.com>
2024-02-22 02:40:12 +0600

    added all of things

--- images/New folder/Web-Dev Re-learning/JS-Learning/qr.py
+++
@@ -1,3 +0,0 @@
-import qrcode
-qrImg = qrcode.make("welcome to to CMT 3rd semester.")
-qrImg.save("qr_img.jpg")
--- images/New folder/Web-Dev Re-learning/JS-Learning/variable.js
+++
@@ -1,7 +0,0 @@
-var studentName = "Suprio Das\n";
-var batch = "CSE 28\n";
-var section = "A\n";
-var id = "CSE 02807546\n";
-var inst = "Port City International University\n";
-
-console.log(studentName, batch, section, id, inst);
